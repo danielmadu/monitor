2d80ffed4fc44722b7f242d98fc1f2b375b53362
danielmadu <daniel.madureira.batista@gmail.com>
2016-05-06 11:55:22 -0300

    Tirada sintaxe ES6

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp');
 
 gulp.task('lint', function() {
   return gulp.src('./lib/*.js')
-    .pipe(jshint());
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('default',['lint']);
